690a40ffb5d22a91cb78b6003036b88569a47fb8
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-05-10 14:43:47 -0600

    fish gif

--- src/components/SuccessPayment/SuccessPayment.tsx
+++ src/components/SuccessPayment/SuccessPayment.tsx
@@ -4,7 +4,7 @@ const SuccessPayment = ({ setStatus }: any) => {
     <div className="lg:w-1/3 w-full border-solid border-2 border-gray-300 px-7 py-12  rounded-2xl bg-white drop-shadow-md">
       <p className="text-center font-mono text-2xl">Pago con éxito</p>
       <div className="my-9 flex justify-center">
-        <Image src="/fish.jpg" alt="check" height={150} width={200} />
+        <Image src="/fish.gif" alt="check" height={150} width={200} />
       </div>
       <div className="text-center">
         <button className="bg-orange-500 px-5 py-2 text-white rounded-lg font-mono" onClick={() => setStatus(0)}>Inicio</button>
