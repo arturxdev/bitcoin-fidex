cb9d28d3cc2928136c8d51f527647d19606c3a29
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-05-12 09:52:52 -0600

    video

--- src/components/SuccessPayment/SuccessPayment.tsx
+++ src/components/SuccessPayment/SuccessPayment.tsx
@@ -4,7 +4,10 @@ const SuccessPayment = ({ setStatus }: any) => {
     <div className="lg:w-1/3 w-full border-solid border-2 border-gray-300 px-7 py-12  rounded-2xl bg-white drop-shadow-md">
       <p className="text-center font-mono text-2xl">Pago con éxito</p>
       <div className="my-9 flex justify-center">
-        <Image src="/fish.gif" alt="check" height={150} width={200} />
+        <video autoPlay loop muted>
+          <source src="/fish.mp4" type="video/mp4" />
+          Tu navegador no soporta videos HTML5.
+        </video>
       </div>
       <div className="text-center">
         <button className="bg-orange-500 px-5 py-2 text-white rounded-lg font-mono" onClick={() => setStatus(0)}>Inicio</button>
